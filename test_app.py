b0b8093365d5e1f851a060a7e26dd52b62ffd021
PhilippHenkelmann <161600832+PhilippHenkelmann@users.noreply.github.com>
2024-10-07 09:54:55 +0200

    Added Jenkinsfile and initial project files

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import main
 class TestApp(unittest.TestCase):
 
     def test_main(self):
-        self.assertEqual(main(),None)
+        self.assertEqual(main(), "This is from feature Branch")  # Erwartet nun den korrekten Rückgabewert
 
-if __name__=="__main__":
+if __name__ == "__main__":
     unittest.main()
